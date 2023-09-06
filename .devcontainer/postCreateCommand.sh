c7fed84bf5e8ffe161afa404c861f045f736a7f3
Jesse Houwing <jesse.houwing@gmail.com>
2023-09-06 23:06:58 +0200

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,3 +4,5 @@ npm install -g docsify-cli
 python -m pip install --upgrade pip setuptools
 python -m pip install --upgrade httpie
 python -m pip install 'urllib3<2'
+
+gh extension install github/gh-valet
