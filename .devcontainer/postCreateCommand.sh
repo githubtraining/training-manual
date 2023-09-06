4d1916e2064ccc70c672b1032dd2a2a490a94d40
Jesse Houwing <jesse.houwing@gmail.com>
2023-09-07 00:02:40 +0200

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,4 +5,13 @@ python -m pip install --upgrade pip setuptools
 python -m pip install --upgrade httpie
 python -m pip install 'urllib3<2'
 
-gh extension install github/gh-valet
+
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+gh extension install github/gh-actions-importer
+gh actions-importer update
